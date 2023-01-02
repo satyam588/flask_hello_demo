ffa9808006f4a7ae1ed0e1bfb4c651bee84198c4
satyam588 <sundaramsatyam12@gmail.com>
2023-01-02 19:10:38 +0530

    initial

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def hello_name():
+    return 'Hello Demo User'
+
+if __name__ == '__main__':
+    app.run()
